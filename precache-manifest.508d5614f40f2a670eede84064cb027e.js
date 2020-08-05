f6a78f0c5174806ac6c4a6b29c4a619e40bf2923
yash <yashodip9@gmail.com>
2020-08-05 10:26:43 +0530

    Updates

---
+++ precache-manifest.508d5614f40f2a670eede84064cb027e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03aa143f478ef6cbf446d908394a6302",
+    "url": "/basic-todo-reactjs/index.html"
+  },
+  {
+    "revision": "e36207019b8069a7cfc3",
+    "url": "/basic-todo-reactjs/static/css/main.5a87e1b4.chunk.css"
+  },
+  {
+    "revision": "2c9d948daa7135ae318e",
+    "url": "/basic-todo-reactjs/static/js/2.fcee669b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-todo-reactjs/static/js/2.fcee669b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36207019b8069a7cfc3",
+    "url": "/basic-todo-reactjs/static/js/main.d15778e6.chunk.js"
+  },
+  {
+    "revision": "23b111caab6e66e38c4e",
+    "url": "/basic-todo-reactjs/static/js/runtime-main.7887bb6d.js"
+  }
+]);
